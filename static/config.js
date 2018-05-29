7ee06cd9763df12435735bdedfd6829f9c77cb02
Lincoln Quick <lincolnmq@gmail.com>
2018-05-29 04:00:05 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1jxCtMeR_vEGTBisezeYeQc1LhQNbFrX6zEIOMdlV_dU";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "1994-1363-4703";
+var inGameName = "Ultra Sun (Lincoln)";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/lincolnquick";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/aa2b1aa0-cb60-11e7-a886-0692ec6181ee.png";
